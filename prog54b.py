e884413a9b83bb992a137f3ffec11c441121153e
LillyA2 <112727168+LillyA2@users.noreply.github.com>
2022-09-11 22:33:07 +0000

    updated

--- prog54b.py
+++ prog54b.py
@@ -4,7 +4,7 @@ num3 = int(input("number 3:"))
 num4 = int(input("number 4:"))
 
 Sum = num1 + num2 + num3 + num4
-average = sum/4
+average = Sum / 4
 
 print("the sum is", Sum)
 print("the average is", average)
