d1c679ed1ac6caed284fab1002b0c0d1fef7412a
LillyA2 <112727168+LillyA2@users.noreply.github.com>
2022-09-11 22:28:27 +0000

    updated

---
+++ prog52a.py
@@ -0,0 +1,10 @@
+length = int( input("Please enter length:"))
+width = int(input("Please enter width:"))
+
+area = length * width
+perim = (2 * length) + (2 * width)
+
+print("the length is", length)
+print("the width is", width)
+print("the area is", area)
+print("the perimeter is", perim)
---
+++ prog54b.py
@@ -0,0 +1,10 @@
+num1 = int(input("number 1:"))
+num2 = int(input("number 2:"))
+num3 = int(input("number 3:"))
+num4 = int(input("number 4:"))
+
+Sum = num1 + num2 + num3 + num4
+average = sum/4
+
+print("the sum is", Sum)
+print("the average is", average)
